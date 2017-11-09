76c27ad1f3ac5079535dac35afa5f525dd89b0ca
Chris Durbin <chrisatumd@gmail.com>
2017-11-08 20:03:50 -0500

    V14: Aggressively go after unprotected docked ships

--- debug_game.sh
+++ debug_game.sh
@@ -2,5 +2,6 @@
 
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Sample.jar"
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Durbinator-v1.0.jar"
-./halite -t -s 1532922776 -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V13.jar"
+#./halite -t -s 1532922776 -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V13.jar"
+./halite -t -s 2219499211 -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V13.jar"
 mv *.hlt hlts/
