5678ae67f5d496f207e2da18fd60c607d883a87b
Chris Durbin <chrisatumd@gmail.com>
2017-11-10 13:28:45 -0500

    V16: Far less willing to dock

--- run_game.sh
+++ run_game.sh
@@ -2,5 +2,5 @@
 
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Sample.jar"
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Durbinator-v1.0.jar"
-./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V14.jar"
+./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V15.jar"
 mv *.hlt hlts/
--- run_game_four_player.sh
+++ run_game_four_player.sh
@@ -2,5 +2,5 @@
 
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Sample.jar"
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Durbinator-v1.0.jar"
-./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V12.jar" "java -jar jars/V13.jar" "java -jar jars/V14.jar"
+./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V15.jar" "java -jar jars/V13.jar" "java -jar jars/V14.jar"
 mv *.hlt hlts/
