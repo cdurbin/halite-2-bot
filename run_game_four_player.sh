fc2f5e921cb75678047564d85b872aef3851ae45
Chris Durbin <chrisatumd@gmail.com>
2017-11-16 16:38:32 -0500

    Update bot versions in run_games

--- run_game_four_player.sh
+++ run_game_four_player.sh
@@ -4,5 +4,5 @@
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/Durbinator-v1.0.jar"
 ## Use this to test early defense
 #./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V15.jar" "java -jar jars/V16.jar" "java -jar jars/V14.jar"
-./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V21.jar" "java -jar jars/V19.jar" "java -jar jars/V20.jar"
+./halite -d "240 160" "java -jar target/MyBot.jar" "java -jar jars/V21.jar" "java -jar jars/V22.jar" "java -jar jars/V20.jar"
 mv *.hlt hlts/
